5795973ff0cef491e1fb211244fc56e3ad85e9bb
Inaki Darville <inaki.darville@student.uclouvain.be>
2024-04-21 00:10:25 +0200

    update cookies

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,  session, redirect, request, url_for
+from flask import Flask, render_template,  session, redirect, request, url_for, make_response
 import os
 from chat_bot.NN import get
 
@@ -12,16 +12,23 @@ def homePage():
     return render_template("index.html")
 
 @app.route('/chat_bot.html',methods=["GET","POST"])
-def show_index():
+def show_chat_bot():
     output = ''
     if request.method == 'POST':
         # Retrieving the value from input box with name='input_name'
+        statut = request.cookies.get('cookie_miams')
+        print(f'Cookie: {statut}')
         input_value = request.form['input_name']
         print(f'You entered: {input_value}')
         output = get(input_value)
-
+        return  render_template('chat_bot.html', output=output)
         #return f'You entered: {input_value}'
-    return render_template('chat_bot.html', output=output)
+    #set the cookies : (key, value)
+    response = make_response(render_template('chat_bot.html'))
+    response.set_cookie('cookie_miams', 'moldu')
+    return response
+    
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
