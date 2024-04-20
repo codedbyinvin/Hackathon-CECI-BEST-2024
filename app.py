6a912d2b60216b55247fa7eb1522de6b335d656d
codedbyinvin <100071469+codedbyinvin@users.noreply.github.com>
2024-04-20 19:54:59 +0200

    ezlaihezi

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
-
-from flask import Flask, request, render_template
-
-app = Flask(__name__)
-
-
-# import hello_world from the hello_world.py
-from hello_world import hello_world
+from flask import Flask, render_template,  session, redirect, request, url_for
+import os
 from NN import get
 
-@app.route('/')
-def hello():
-    return hello_world()
 
-@app.route('/form', methods=['GET', 'POST'])
-def form():
+IMG_FOLDER = os.path.join('static', 'images')
+app = Flask(__name__)
+app.config['IMG_FOLDER'] = IMG_FOLDER
+
+@app.route('/',methods=["GET"])  
+def homePage():
+    return render_template("index.html")
+
+@app.route('/chat_bot.html',methods=["GET","POST"])
+def show_index():
     output = ''
     if request.method == 'POST':
         # Retrieving the value from input box with name='input_name'
         input_value = request.form['input_name']
-        #print(f'You entered: {input_value}')
+        print(f'You entered: {input_value}')
         output = get(input_value)
 
         #return f'You entered: {input_value}'
-    return render_template('form.html', output=output)
+    return render_template('chat_bot.html', output=output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- hello_world.py
+++
@@ -1,5 +0,0 @@
-
-# flask hello world file
-def hello_world():
-    return 'Hello, World! new'
-# Path: app.py
